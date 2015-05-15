af6bed861ffee7be089eef95848983166fc54a26
Jack Moore <hello@jacklmoore.com>
2015-05-15 02:55:11 -0400

    Breaking change:     radio and checkbox elements now return their value attribute value, instead of a boolean.
    Refactor:
        Having all elements return their value simplified the code needed.
        Replaced checking closest ancestor with `matches(':disabled')`, which should be more accurate and faster.  Using the closest element with the disabled attribute could give a false positive if the element was not a valid target for the disabled attribute.

--- index.js
+++ index.js
@@ -1,46 +1,21 @@
-
 /* jshint -W093 */
 
-var closest = require('component-closest')
-
-var slice = [].slice
+var matches = require('matches-selector')
 
 module.exports = function (form) {
   var body = Object.create(null)
 
-  slice.call(form.querySelectorAll('input, textarea, select'))
+  Array.prototype.slice.call(form.querySelectorAll('input, textarea, select'))
   .forEach(function (el) {
     // if an element has no name, it wouldn't be sent to the server
     if (!el.name) return
     // if an element is read only, then it doesn't make sense to send client
     if (el.readOnly) return
-    // if the element is disabled or any ancestor is disabled,
-    // then it shouldn't be sent to the server
-    if (closest(el, '[disabled]', true)) return
+    // if the element is disabled then it wouldn't be sent to the server
+    if (matches(el, ':disabled')) return
 
-    switch (el.tagName.toLowerCase()) {
-      case 'textarea': return body[el.name] = el.value
-      case 'select':
-        if (el.options.length) body[el.name] = el.options[el.selectedIndex].value
-        return
-      case 'input': break
-      default: return
-    }
+    if (el.type.indexOf(['file', 'reset', 'submit', 'button']) !== -1) return
 
-    switch (el.type) {
-      // not supported types
-      case 'file':
-      case 'reset':
-      case 'submit':
-      case 'button':
-        return
-      case 'checkbox': return body[el.name] = !!el.checked
-      case 'radio':
-        if (el.selected) body[el.name] = el.value
-        return
-    }
-
-    // otherwise we assume it's text
     body[el.name] = el.value
   })
 

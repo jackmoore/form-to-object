43c027cacf27509c70ff2e2a934114171dcfa468
Jack Moore <hello@jacklmoore.com>
2015-07-28 11:34:40 -0400

    Breaking change: If multiple fields have the same name, store those values as a array.  This matches the typical behavior in node for form data parsing (body-parser and formidable).

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ module.exports = function (form) {
 
     if (el.type.indexOf(['file', 'reset', 'submit', 'button']) !== -1) return
 
-    body[el.name] = el.value
+    if (body[el.name] === undefined) {
+      body[el.name] = el.value;
+    } else if (Object.prototype.toString.call(body[el.name]) === '[object Array]') {
+      body[el.name].push(el.value);
+    } else {
+      body[el.name] = [body[el.name], el.value];
+    }
   })
 
   return body

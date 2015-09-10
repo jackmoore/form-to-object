07a73b7a593e34a965084a92b1049ad0b12e24fa
Jack Moore <hello@jacklmoore.com>
2015-09-10 04:50:55 -0400

    Using CSS selector to exclude disabled fields - drops dependency, but also IE8 support for excluding disabled fields.

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 /* jshint -W093 */
 
-var matches = require('matches-selector')
-
 module.exports = function (form) {
   var body = Object.create(null)
 
-  Array.prototype.slice.call(form.querySelectorAll('input, textarea, select'))
+  Array.prototype.slice.call(form.querySelectorAll('input:not(:disabled), textarea:not(:disabled), select:not(:disabled)'))
   .forEach(function (el) {
     // if an element has no name, it wouldn't be sent to the server
     if (!el.name) return
     // if an element is read only, then it doesn't make sense to send client
     if (el.readOnly) return
-    // if the element is disabled then it wouldn't be sent to the server
-    if (matches(el, ':disabled')) return
 
     if (el.type.indexOf(['file', 'reset', 'submit', 'button']) !== -1) return
 

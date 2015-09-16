841f9664a2ca7d8f7d6b6028cf4146e7fd63f3ba
Jack Moore <hello@jacklmoore.com>
2015-09-16 01:59:07 -0400

    Fixed issue with non-checked radio and checkbox input values being sent. Safer checking of element type. Added support for explicitly putting values in an array by connoting array notation in the element name: e.g. <input type='checkbox' name='state[]' value='CA'/> results in {state: ['CA']}

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 module.exports = function (form) {
   var body = Object.create(null)
 
-  Array.prototype.slice.call(form.querySelectorAll('input:not(:disabled), textarea:not(:disabled), select:not(:disabled)'))
-  .forEach(function (el) {
+  Array.prototype.slice.call(form.querySelectorAll('input:not(:disabled), textarea:not(:disabled), select:not(:disabled)')).forEach(function (el) {
+    var key = el.name;
+
     // if an element has no name, it wouldn't be sent to the server
-    if (!el.name) return
+    if (!key) return
+
     // if an element is read only, then it doesn't make sense to send client
     if (el.readOnly) return
 
-    if (el.type.indexOf(['file', 'reset', 'submit', 'button']) !== -1) return
+    if (['file', 'reset', 'submit', 'button'].indexOf(el.type) > -1) return
 
-    if (body[el.name] === undefined) {
-      body[el.name] = el.value;
-    } else if (Object.prototype.toString.call(body[el.name]) === '[object Array]') {
-      body[el.name].push(el.value);
+    if (['checkbox', 'radio'].indexOf(el.type) > -1 && !el.checked) return
+
+    if (/\[\]$/.test(key)) {
+      key = key.slice(0,-2);
+
+      // if using array notation, go ahead and put the first value into an array.
+      if (body[key] === undefined) {
+        body[key] = [];
+      }
+    }
+    
+    if (body[key] === undefined) {
+      body[key] = el.value;
+    } else if (Object.prototype.toString.call(body[key]) === '[object Array]') {
+      body[key].push(el.value);
     } else {
-      body[el.name] = [body[el.name], el.value];
+      body[key] = [body[key], el.value];
     }
   })
 
